d0f288fadeb407e0a1065547fca45778e621fec1
Robert Blackwell <reblackwell3@gmail.com>
2024-09-26 09:13:48 -0400

    remove games settings, remove index.d.ts

--- src/index.d.ts
+++
@@ -1,4 +0,0 @@
-// export * from './settings/coursesSettings';
-export * from './settings/gamesSettings';
-export * from './settings/mistakesSettings';
-export * from './settings/puzzleSettings';
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
-// export * from './settings/coursesSettings';
-export * from './settings/gamesSettings';
 export * from './settings/mistakesSettings';
 export * from './settings/puzzleSettings';
